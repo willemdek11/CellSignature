1d5316fb04d8ba332abfeae2214d407f3ffb4bbe
unknown <willemdek@live.nl>
2019-06-04 11:46:31 +0200

    delete subfolder

--- CellSignature/R/pp_exp.R
+++
@@ -1,8 +0,0 @@
-pp_exp <- function (exp)
-{
-  sig <- sig
-  g <- Reduce(intersect, list(as.character(row.names(exp),
-                                           sig$gene)))
-  exp <- exp[as.character(g), ]
-  return(exp)
-}
--- CellSignature/R/pp_sig.R
+++
@@ -1,7 +0,0 @@
-pp_sig <- function (exp)
-{
-  g <- Reduce(intersect, list(as.character(row.names(exp),
-                                           sig$gene)))
-  sig <- sig[sig$gene %in% as.character(g), ]
-  return(sig)
-}
