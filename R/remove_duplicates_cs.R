7abd70cec5feb8a96ee3e21882be9c26259683df
unknown <willemdek@live.nl>
2019-08-21 11:29:40 +0200

    Remove genes from less correlated groups, to keep one unique group.

---
+++ R/remove_duplicates_cs.R
@@ -0,0 +1,36 @@
+#' @title Feature selection of signature genes
+#' @description Signature genes are expected to be co-expressed in tissue transcriptomic data. However, depending on the dataset some of the genes may not co-express with the dominant module. In order to remove such deviant genes, a feature selection can be carried out based on correlation. This function removes genes that exhibit a poor correlation (less than the defined r value) with in the experimental data. This step of feature selection is recommended to enrich the prediction of relative abundance of immune cells.
+#' @param exp Dataframe of transcriptomic data (natural scale) containing genes as rows and samples as columns. Note: Gene names should be set as row names and duplicates are not allowed. Missing values are not allowed within the expression matrix. Check example- head(example_data): \code{\link{example_data}}.
+#' @param sig Dataframe of Cell Signature containing genes and their cell signature in rows.
+#' @param r Use a value between 0 and 1. Default is 0.6. This is a user defined correlation cut-off to perform feature selection. To get an idea of what cut-off to use check the results of (\code{\link{gene_stat}}) and choose a cut-off that displays high median correlation and maintains a high proportion of genes after feature selection.
+#' @return Returns a table of 'feature selected' genes based on the set r value.
+#' @examples
+#' feature_select_cs (exp = example_data, sig = signature_table, r = 0.7)
+#' @export
+
+remove_duplicates_cs <- function(exp,sig){
+  exp <- pp_exp(exp, sig)
+  sig <- pp_sig(exp, sig)
+
+  fg_duplicates <- sig[duplicated(sig$gene) | duplicated(sig$gene, fromLast=TRUE),] %>% droplevels()
+  fg_all <- anti_join(sig, fg_duplicates, by = c("gene" = "gene"))
+
+  cor_data <- fastCor_cs(t(exp))
+
+  for (i in levels(fg_duplicates$gene)){
+    corr_max <- 0
+    for (j in fg_duplicates[fg_duplicates$gene == i, "cell"]){
+      corr_subb <- pp_cor(cor_data, sig, j)
+      corr_temp <- (sum(corr_subb[,i]))/length(corr_subb[,i])
+      if (corr_temp > corr_max){
+        corr_max <- corr_temp
+        type_max <- j
+      }
+    }
+    fg_subb <- data.frame(i, type_max)
+    fg_all <- rbindlist(list(fg_all, fg_subb), use.names = FALSE)
+    }
+  colnames(fg_all) <- c("gene", "cell")
+  fg_all <- data.frame(lapply(fg_all, as.character), stringsAsFactors = F)
+  return(fg_all)
+}
